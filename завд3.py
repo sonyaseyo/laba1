85bf028eb3f5f7509fa091683b6eb3221efb9837
sonya <sonyaseyooo@gmail.com>
2021-09-12 14:07:14 +0300

    поменяла исключение

--- завд3.py
+++ завд3.py
@@ -4,12 +4,10 @@ def hello():
     print('Чумак С.І., група КМ-12')
 
 def read_x():
-    print ('Введіть значення х')
-    x=float(input())
+    x=float(input('Введіть значення х'))
     return x
 
 def calculate_y():
-    x=read_x()
     if x<-1.5 :
         y=math.pi*math.sin(x)
     else :
@@ -24,12 +22,12 @@ if __name__ == "__main__":
     hello()
     while True:
         try:
-            read_x()
-            calculate_y()
-            (x,y)=calculate_y()
+            x=read_x()
+            y=calculate_y(x)
             print ("При значенні х = %f y = %f." % (x,y))
             break
-        except : print ('Введено не число!')
+        except Exception as e:
+            print (e)
 
 
 
